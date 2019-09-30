4d984f96da9baaea4d292265f33c3f997c0cce32
Josh F <fowlerjosh93@gmail.com>
2019-09-30 17:20:40 -0400

    added functions for the bandsintown search and the omdb search

--- liri.js
+++ liri.js
@@ -1,19 +1,110 @@
+// /declare variables for dependencies
+
 require("dotenv").config();
+//use to request data from bandsintown and omdb
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-var artist = process.argv[2];
-var concert = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-// var song = 
+//format date and time for concerts
+var moment = require("moment")
+//declaring globally for formatting purposes
+var divider = "\n------------------------------------------------------------\n\n";
+//require fs for appending file
+var fs = require("fs");
 
-console.log(concert);
+//take user input
+var search = process.argv[2];
+var term = process.argv.slice(3).join(" ");
 
-axios
-.get(concert)
-.then(function(response) {
-    console.log(response.data.venue);
-    console.log(response.region);
-    console.log(response.city);
-    console.log(response.datetime);
+//switch to process commands
+
+switch (search) {
+  case "concert-this":
+    concertSearch(term);
+    break;
+  case "spotify-this-song":
+    spotifySearch(term);
+    break;
+  case "movie-this":
+    movieSearch(term);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+};
+
+//function to search bandsintown
+function concertSearch(term) {
+  var artist = term;
+  var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+  axios.get(url).then(
+    function (response) {
+      // console.log(response.data)
+
+      var jsonData = response.data;
+      for (var i = 0; i < jsonData.length; i++) {
+        var concertData = [
+          "Artist Name: " + jsonData[i].lineup,
+          "Concert Time: " + moment(jsonData[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'),
+          "Concert Location: " + jsonData[i].venue.city + "," + jsonData[i].venue.region + "," + jsonData[i].venue.country,
+          "Concert Venue: " + jsonData[i].venue.name,
+        ].join("\n\n");
+        fs.appendFile("log.txt", concertData + divider, function (err) {
+          if (err) throw err;
+          console.log(concertData);
+        });
+      }
+    });
+};
+
+//function to search omdb for movies
+function movieSearch(term) {
+  var movie = term;
+  if (!movie) {
+      console.log("If you haven't watched 'Mr. Nobody,' then you should.")
+      console.log("It's on Netflix!")
+      // movie = "Mr. Nobody"
+  }
+  var url = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  axios.get(url).then(
+      function (response) {
+      console.log(response.data)
+
+      var jsonData = response.data;
+        var movieData = [
+          "Movie Title: " + jsonData.Title,
+          "Year: " + jsonData.Year,
+          "IMDB Rating: " + jsonData.imdbRating,
+          "Rotten Tomatoes: " + jsonData.Ratings[1].Value,
+          "Country: " + jsonData.Country,
+          "Language: " + jsonData.Language,
+          "Plot: " + jsonData.Plot,
+          "Actors: " + jsonData.Actors
+        ].join("\n\n");
+        fs.appendFile("log.txt", movieData + divider, function (err) {
+          if (err) throw err;
+          console.log(movieData);
+        });
+      })
+};
+
+//function to request song from spotify
+
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    //Return error if error occurs.
+    if (error) {
+      return console.log(error);
+    }
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+
+    if (dataArr[0] === "spotify-this-song") {
+      var songcheck = dataArr[1].slice(1, -1);
+      console.log("Song Check: " + songcheck)
+      spotifySearch(songcheck);
+    }
   })
+}
